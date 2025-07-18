9010f4d027218590ea124e3ec10ca5b871fc9841
Thoms <pro.thomasgiang@gmail.com>
2025-07-18 02:44:24 +0200

    extraction class created with basic load functions + main file to test it. setting up venv for libraries

---
+++ src/extraction/extraction.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import os
+import json
+
+class extraction:
+    def __init__(self, data_folder: str):
+        self.data_folder = data_folder
+        self.clinical_trials = None
+        self.drugs = None
+        self.pubmed_csv = None
+        self.pubmed_json = None
+        self._load_data()
+
+    def _load_data(self):
+        # Charger clinical_trials.csv
+        clinical_trials_path = os.path.join(self.data_folder, "clinical_trials.csv")
+        self.clinical_trials = pd.read_csv(clinical_trials_path)
+        #print(self.clinical_trials.head())
+
+        # Charger drugs.csv
+        drugs_path = os.path.join(self.data_folder, "drugs.csv")
+        self.drugs = pd.read_csv(drugs_path)
+        #print(self.drugs.head())
+
+        # Charger pubmed.csv
+        pubmed_csv_path = os.path.join(self.data_folder, "pubmed.csv")
+        self.pubmed_csv = pd.read_csv(pubmed_csv_path)
+        #print(self.pubmed_csv.head())
+
+        # Charger pubmed.json
+        pubmed_json_path = os.path.join(self.data_folder, "pubmed_corrected.json")
+        with open(pubmed_json_path, encoding="utf-8") as f:
+            self.pubmed_json = pd.DataFrame(json.load(f))
+        print(self.pubmed_json.head()) 
---
+++ src/main.py
@@ -0,0 +1,3 @@
+from extraction.extraction import extraction
+
+extract = extraction("data\input")
