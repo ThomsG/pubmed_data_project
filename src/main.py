a9dcb6334edeed873b7f452a182af6bf86c80258
Thoms <pro.thomasgiang@gmail.com>
2025-07-18 04:24:43 +0200

    verification step before extracting data

--- src/main.py
+++ src/main.py
@@ -1,3 +1,22 @@
-from extraction.extraction import extraction
+from extraction.extraction import Extraction
+from verification.verification import Verification
 
-extract = extraction("data\input")
+# Checking file validity
+print("Checking file validity...")
+
+invalid_files = Verification("data/input")
+if invalid_files:
+    print("Invalid files:", invalid_files)
+else:
+    print("All files are valid.")
+
+# Extracting data from input files
+print("Loading data...")
+
+extract = Extraction("data\input")
+
+print(extract.clinical_trials.head())
+print(extract.drugs.head())
+print(extract.pubmed_csv.head())
+print(extract.pubmed_json.head())
+
